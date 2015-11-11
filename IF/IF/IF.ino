4fb561816a3908648b1c32306bc7a2c73a2b5f48
luooove <644175554@qq.com>
2015-11-11 20:08:43 +0800

    add if

---
+++ IF/IF/IF.ino
@@ -0,0 +1,38 @@
+/*
+AND OR
+
+  modified 11 November 2015
+  by xiaoxiang luo
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(3, INPUT);
+  pinMode(4, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(3) == 1) // AND Mode
+  {
+
+    digitalWrite(1, HIGH);
+  }
+  else  //OR Mode
+  {
+    digitalWrite(1, LOW);
+  }
+
+  if (digitalRead(4) == 1) // AND Mode
+  {
+
+    digitalWrite(0, HIGH);
+  }
+  else  //OR Mode
+  {
+    digitalWrite(0, LOW);
+  }
+}
