6acb0836b87fdebbc6cc40edcf3692508022a17c
luooove <644175554@qq.com>
2015-12-05 21:21:09 +0800

    ad
    a

--- AnalogOut/AnalogOut.ino
+++ AnalogOut/AnalogOut.ino
@@ -1,13 +1,27 @@
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(0,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //analogWrite(0,analogRead(2)/4);
-  if (digitalRead(3) == 1)
-    analogWrite(0, analogRead(2) / 4); //some problem in this pin
-  else
-    analogWrite(0, 0);
+//  if (digitalRead(3) == 1)
+//    analogWrite(0, analogRead(2) / 4); //some problem in this pin
+//  else
+//    analogWrite(0, 0);
+
+int a=analogRead(2);
+if(a>950)
+digitalWrite(0,HIGH);
+else if(a<75)
+digitalWrite(0,LOW);
+else
+{
+  digitalWrite(0,HIGH);
+delayMicroseconds(a*34);
+digitalWrite(0,LOW);
+delayMicroseconds(35000-a*34);
+  }
+
 }
---
+++ Servo/Servo.ino
@@ -0,0 +1,85 @@
+//Servo test ok! also like a fade LED
+
+//Author: Leo
+//Date: 2015.03.12
+//IDE Ver.: 1.0.3
+//website: github.com/leolite
+//MCU: ATMEL ATTINY13
+//Programmer: usbtinyisp(@Ladyada https://learn.adafruit.com/usbtinyisp/drivers)
+//hardware core: core13(@smeezekitty http://sourceforge.net/projects/ard-core13/)
+
+/*
+
+ This example code is in the public domain.
+ */
+
+//       ATMEL ATTINY13 / ARDUINO
+//
+//                      +-\/-+
+// [RST]ADC0 (D 5) PB5 1|    |8 Vcc
+//      ADC3 (D 3) PB3 2|    |7 PB2 (D 2) ADC1[SCK]
+//      ADC2 (D 4) PB4 3|    |6 PB1 (D 1) PWM1[MISO]
+//                 GND 4|    |5 PB0 (D 0) PWM0[MOSI]
+//                      +----+
+
+//Timer define
+int t = 0;
+int period = 120;
+int pulseWidthScaled = 25;
+//User data
+int servoPin = 0;
+int pulseStartPoint = 5; //MIN start position is 5
+int pulseEndPoint = 25;  //MAX end position is 25
+int overflowValueCTC = 10;
+
+void setup(){
+  pinMode(servoPin, OUTPUT);   
+  setup_timer(); // Initialize the timer
+}
+
+void loop() {
+int a=analogRead(2);
+a=a/51+5;
+pulseWidthScaled = a;
+}
+
+// TIMER0 -- Only one in attiny13
+void setup_timer(void) {
+  //timer 1:
+  TCCR0A = 0;// set entire TCCR1A register to 0
+  TCCR0B = 0;// same for TCCR1B
+
+  // Set up the timer 
+  TCCR0A |= (1 << WGM01);  // Mode: CTC
+  TCCR0B |= (1 << CS01); // Prescaler: 
+
+  // Initialize the count
+  TCNT0 = 0;
+
+  // Initialize the compare value
+  OCR0A = overflowValueCTC;
+
+  // Enable compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+
+  // Enable global interrupts
+  sei();
+}
+
+//timer 1 interrupt
+ISR (TIM0_COMPA_vect) {
+  //increment t and reset each time it reaches period
+  t += 1;
+  if (t >= period){
+    t = 0;
+  }
+  if (pulseWidthScaled <= t) {
+    digitalWrite(servoPin, LOW);
+  }
+  else{
+    digitalWrite(servoPin, HIGH);
+  }
+}
+
+
+
